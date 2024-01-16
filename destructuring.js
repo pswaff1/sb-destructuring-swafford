434d50a4f5b25480249c7a65e2e8a69c0cc243f4
Patrick Swafford <patrick.swafford@swaffordhouse.com>
2024-01-15 22:08:19 -0500

    obj_destructuring_3 implemented

--- destructuring.js
+++ destructuring.js
@@ -4,21 +4,20 @@
 
 // Object Destructuring 1
 const Obj_Destructuring_1 = () => {
-    
     // Declares an object with two attributes: numPlanets and
     // yearNeptuneDiscovered and sets them to 8 and 1846
     // respectively
     let facts = {
         numPlanets: 8,
-        yearNeptuneDiscovered: 1846
+        yearNeptuneDiscovered: 1846,
     };
-    
+
     // Declares two variables, numPlanets and
     // yearNeptune Discoved. Destructuring is used to assign
-    // to these variables the values assigned to the 
+    // to these variables the values assigned to the
     // corresponding attributes in the facts object, namely
     // 8 and 1846, respectively
-    let {numPlanets, yearNeptuneDiscovered} = facts;
+    let { numPlanets, yearNeptuneDiscovered } = facts;
 
     // prints the value stored in numPlanets, 8, to the console
     console.log(numPlanets);
@@ -26,26 +25,51 @@ const Obj_Destructuring_1 = () => {
     // prints the value stored in yearNeptuneDiscovered, 1846,
     // to the console
     console.log(yearNeptuneDiscovered);
-}
+};
 
 // Object Destructuring 2
 const Obj_Destructuring_2 = () => {
-    // Declares an object with three attributes: numPlanets, 
-    // yearNeptuneDiscovered, and yearMarsDiscoverd, and sets 
+    // Declares an object with three attributes: numPlanets,
+    // yearNeptuneDiscovered, and yearMarsDiscoverd, and sets
     // them to 8, 1846, and 1659 respectively
     let planetFacts = {
         numPlanets: 8,
         yearNeptuneDiscovered: 1846,
-        yearMarsDiscovered: 1659
+        yearMarsDiscovered: 1659,
     };
 
     // Destructures numPlanets from PlanetFacts into its own
-    // variable, the remaining attributes are destructured into 
+    // variable, the remaining attributes are destructured into
     // discoveryYears array using the rest operator
-    let {numPlanets, ...discoveryYears} = planetFacts;
+    let { numPlanets, ...discoveryYears } = planetFacts;
 
     // prints the discoveryYears array onto the console. This
     // array contains yearNeptuneDiscoverd which is 1846 and
     // yearMarsDiscovered, which is 1659
     console.log(discoveryYears);
-}
+};
+
+// Object Destructuring 3
+// Function that accepts two parameters, firstName and 
+// favoriteColor and returns a prompt telling the user their
+// name and their favorite color. If no firstName is passed
+// in, firstName is undefined. if favoriteColor is not passed
+// in, it defaults to green.
+const getUserData = ({ firstName, favoriteColor = "green" }) => (
+    `Your name is ${firstName} and you like ${favoriteColor}`
+);
+
+const Obj_Destructuring_3 = () => {
+    // prints message with firstName=Alejandro and favoriteColor
+    // being purple
+    console.log(getUserData({ firstName: "Alejandro", favoriteColor: "purple" }));
+    
+    // prints message with firstName being Melissa and
+    // favoriteColor being green, the default
+    console.log(getUserData({ firstName: "Melissa" }));
+
+    // prints message with firstName being undefined as no
+    // default was defined and favoriteColor being green, the 
+    // default.
+    console.log(getUserData({}));
+};
